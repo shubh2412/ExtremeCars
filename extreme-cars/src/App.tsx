c8afa2ba3ae1d90fe9f8d5b66b55e362c9970032
shubh2412 <shubhrkumar@deloitte.com>
2022-12-05 20:41:19 +0530

    intial commit

---
+++ extreme-cars/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Extreme Cars</h1>
+     
+    </div>
+  );
+}
+
+export default App;
